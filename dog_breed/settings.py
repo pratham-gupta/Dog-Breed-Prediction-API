b3ebd04a4c1685cb0bc4b2c43ee70a36c1ad8bfc
Pratham Gupta <pratham04031999@gmail.com>
2021-02-27 02:23:19 +0530

    added hosts

--- dog_breed/settings.py
+++ dog_breed/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SAVED_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dog-breed-predictor.herokuapp.com']
 
 
 # Application definition
